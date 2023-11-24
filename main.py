1f10f70206ef6bca14d1fd00c0ff968969de5f4f
Joanna <joannalewicka@outlook.com>
2023-11-24 18:06:38 +0100

    Button start/stop

--- main.py
+++ main.py
@@ -34,12 +34,46 @@ digits = [
     [0, 0, 0, 0, 0, 0, 0, 1], # 8
     [0, 0, 0, 0, 1, 0, 0, 1], # 9
 ]
+
+button = Pin(21, Pin.IN, Pin.PULL_UP)
+buttonPressed = 1 # Not pressed at start
+OnOff = 1
+nextNumber = 0
+
 def clear():
     for i in pins:
         i.value(0)
+        
+def waitAndDetectButtonPress(seconds):
+    global OnOff
+    waitTime = seconds * 100
+    while waitTime > 0:
+        if button.value() == 0:
+            if OnOff == 0:
+                OnOff = 1
+            else:
+                OnOff = 0
+            utime.sleep(1)
+            break
+        else:
+            utime.sleep(0.01)
+            waitTime = waitTime - 1
+
 clear()
+
 while True:
-    for i in range(len(digits)):
-        for j in range(len(pins)-1):
-            pins[j].value(digits[i][j])
-        utime.sleep(1)
+    if OnOff == 1: # On
+        i = nextNumber
+        while i <= 9:
+            if OnOff == 1:
+                for j in range(len(pins)-1):
+                    pins[j].value(digits[i][j])
+                if i == 9:
+                    nextNumber = 0
+                else: 
+                    nextNumber = i + 1
+                i = nextNumber
+                waitAndDetectButtonPress(1)
+            else:
+                break
+    waitAndDetectButtonPress(1)
