f8c579688dd0a4db44fb98dc3c45bcdef9117b17
Joanna <joannalewicka@outlook.com>
2023-11-24 21:19:16 +0100

    Buttons for start/stop, reverse and reset

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from machine import Pin
 import utime
 
+# global params
+buttonStartStop = Pin(11, Pin.IN, Pin.PULL_UP) # Start/Stop
+buttonReverse = Pin(10, Pin.IN, Pin.PULL_UP) # Reverse
+buttonReset = Pin(9, Pin.IN, Pin.PULL_UP) # Reset
+buttonManual = Pin(8, Pin.IN, Pin.PULL_UP) # Manual
+ledYellow = Pin(21, Pin.OUT) #Start/Stop LED
+ledGreen = Pin(20, Pin.OUT) #Reverse LED
+ledRed = Pin(22, Pin.OUT) #Reset LED
+buttonPressed = 1 # PULL_UP so 1 is not pressed
+onOff = 0 # by default don't start the counter
+reverse = 0 # by default forword
+nextNumberPlus = 1
+nextNumberMinus = 9
+
 # 7-segment display layout
 #       A
 #      ---
@@ -35,45 +49,119 @@ digits = [
     [0, 0, 0, 0, 1, 0, 0, 1], # 9
 ]
 
-button = Pin(21, Pin.IN, Pin.PULL_UP)
-buttonPressed = 1 # Not pressed at start
-OnOff = 1
-nextNumber = 0
-
 def clear():
     for i in pins:
-        i.value(0)
+        i.value(1)
+        
+def setZero():
+    for j in range(len(pins)-1):
+        pins[j].value(digits[0][j]) 
+         
         
 def waitAndDetectButtonPress(seconds):
-    global OnOff
+    global onOff
+    global reverse
+    global nextNumberMinus
+    global nextNumberPlus
     waitTime = seconds * 100
     while waitTime > 0:
-        if button.value() == 0:
-            if OnOff == 0:
-                OnOff = 1
+        if buttonStartStop.value() == 0:
+            ledYellow.toggle()
+            if onOff == 0:
+                print("Start")
+                onOff = 1
             else:
-                OnOff = 0
-            utime.sleep(1)
+                print("Stop")
+                onOff = 0
+            while buttonStartStop.value() == 0:
+                utime.sleep(0.01)
+            break
+        elif buttonReverse.value() == 0:
+            ledGreen.toggle()
+            if reverse == 0:
+                print("Backwards count")
+                reverse = 1
+            else:
+                print("Fowrward count")
+                reverse = 0
+            while buttonReverse.value() == 0:
+                utime.sleep(0.01)
+            break
+        elif buttonReset.value() == 0:
+            ledRed.toggle()
+            print("Reset")
+            setZero()
+            if onOff == 1:
+                ledYellow.toggle()
+            onOff = 0
+            if reverse == 1:
+                ledGreen.toggle()
+            reverse = 0
+            nextNumberPlus = 1
+            nextNumberMinus = 9
+            while buttonReset.value() == 0:
+                utime.sleep(0.5)
+            ledRed.toggle()
             break
         else:
             utime.sleep(0.01)
             waitTime = waitTime - 1
 
-clear()
+def countFunction():
+    global nextNumberPlus
+    global nextNumberMinus
+    while reverse == 0:
+        #print("Reverse 0")
+        if onOff == 1: # On
+            i = nextNumberPlus
+            while i <= 9:
+                #print(nextNumberPlus)
+                if onOff == 1 and reverse == 0:
+                    for j in range(len(pins)-1):
+                        pins[j].value(digits[i][j])
+                    if i == 9:
+                        nextNumberPlus = 0
+                    else: 
+                        nextNumberPlus = i + 1
+                        nextNumberMinus = i
+                    i = nextNumberPlus
+                    waitAndDetectButtonPress(1)
+                else:
+                    break
+        else:
+            waitAndDetectButtonPress(1)
+        
+def countBackwordsFunction():
+    global nextNumberMinus
+    global nextNumberPlus
+    while reverse == 1:
+        #print('Reverse 1')
+        if onOff == 1: # On
+            i = nextNumberMinus
+            while i >= 0:
+                #print(nextNumberMinus)
+                if onOff == 1 and reverse == 1:
+                    for j in range(len(pins)-1,-1,-1):
+                        pins[j].value(digits[i][j])
+                    if i == 0:
+                        nextNumberMinus = 9
+                    else: 
+                        nextNumberMinus = i - 1
+                        nextNumberPlus = i
+                    i = nextNumberMinus
+                    waitAndDetectButtonPress(1)
+                else:
+                    break
+        else:
+            waitAndDetectButtonPress(1)    
 
-while True:
-    if OnOff == 1: # On
-        i = nextNumber
-        while i <= 9:
-            if OnOff == 1:
-                for j in range(len(pins)-1):
-                    pins[j].value(digits[i][j])
-                if i == 9:
-                    nextNumber = 0
-                else: 
-                    nextNumber = i + 1
-                i = nextNumber
-                waitAndDetectButtonPress(1)
-            else:
-                break
-    waitAndDetectButtonPress(1)
+# Start run
+setZero()
+
+while True: 
+    if reverse == 0:
+        countFunction()
+    else:
+        countBackwordsFunction()
+
+
